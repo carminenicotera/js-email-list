e6de8a6f67725f89cd38b7e4877a1aa733f7bb52
carminenicotera <carmine.91.n@hotmail.it>
2026-02-23 16:29:03 +0100

    chiamati API per generare solo un'email

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ const listEl = document.querySelector('.list');
 
 //creazione endpoint in cui salvo l'indirizzo delle API di Boolean
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'; 
+
+//chiamata alle API con fetch
+fetch(endpoint)
+  .then(result => result.json())
+  .then(data => {
+    console.log(data);
+    const email = data.response
+    const markup = `<li>${email}</li>`
+    listEl.innerHTML = markup
+  }).catch(err => {
+    console.log(err.message);
+  }).finally(() => {
+    console.log('end');
+  })
