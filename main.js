5b5be5be56847af8d38bdea9a8dd1af1c419746d
carminenicotera <carmine.91.n@hotmail.it>
2026-02-23 17:34:39 +0100

    aggiunto bottone che genere al click 10 email random

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 //seleziono nodo del dom
 const listEl = document.querySelector('.list');
+const buttonEl = document.querySelector('.btn')
 
 //creazione endpoint in cui salvo l'indirizzo delle API di Boolean
-const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'; 
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-//ciclo for per generare 10 email casuali
-for (let i = 0; i < 10; i++) {
-  //chiamata alle API con fetch
-  fetch(endpoint)
-    .then(result => result.json())
-    .then(data => {
-      //console.log(data);
-      const email = data.response
-      //console.log(email);
-      const markup = `<li>${email}</li>`
-      listEl.innerHTML += markup
-    }).catch(err => {
-      console.log(err.message);
-    }).finally(() => {
-      console.log('end');
-    })
-}
+
+buttonEl.addEventListener('click', function () {
+  //ciclo for per generare 10 email casuali
+  listEl.innerHTML = ''
+  for (let i = 0; i < 10; i++) {
+    //chiamata alle API con fetch
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(data => {
+        //console.log(data);
+        const email = data.response
+        //console.log(email);
+        const markup = `<li>${email}</li>`
+        listEl.innerHTML += markup;
+      }).catch(err => {
+        console.log(err.message);
+      }).finally(() => {
+        console.log('end');
+      })
+  }
+})
