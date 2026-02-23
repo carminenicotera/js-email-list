373e3327f0f955414065c0819ddfb50eafcc8efc
carminenicotera <carmine.91.n@hotmail.it>
2026-02-23 17:02:19 +0100

    inserisco for per generare in pagina 10 email random

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ const listEl = document.querySelector('.list');
 //creazione endpoint in cui salvo l'indirizzo delle API di Boolean
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'; 
 
-//chiamata alle API con fetch
-fetch(endpoint)
-  .then(result => result.json())
-  .then(data => {
-    console.log(data);
-    const email = data.response
-    const markup = `<li>${email}</li>`
-    listEl.innerHTML = markup
-  }).catch(err => {
-    console.log(err.message);
-  }).finally(() => {
-    console.log('end');
-  })
+//ciclo for per generare 10 email casuali
+for (let i = 0; i < 10; i++) {
+  //chiamata alle API con fetch
+  fetch(endpoint)
+    .then(result => result.json())
+    .then(data => {
+      //console.log(data);
+      const email = data.response
+      //console.log(email);
+      const markup = `<li>${email}</li>`
+      listEl.innerHTML += markup
+    }).catch(err => {
+      console.log(err.message);
+    }).finally(() => {
+      console.log('end');
+    })
+}
