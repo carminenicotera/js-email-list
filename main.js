aae7cc16c8385dc0562f9bd99e1a6e9d6abbe6cd
carminenicotera <carmine.91.n@hotmail.it>
2026-02-23 15:58:33 +0100

    inserita  <ul> e selezionato il nodo , creato endpoint

---
+++ main.js
@@ -0,0 +1,4 @@
+//seleziono nodo del dom
+const listEl = document.querySelector('.list');
+//creazione endpoint in cui salvo l'indirizzo delle API di Boolean
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'; 
